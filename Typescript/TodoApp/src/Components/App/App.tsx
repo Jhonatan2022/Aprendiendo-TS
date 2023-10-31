4280efa34b0c25844fb6806cbacc8426ed6281f1
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-10-31 15:03:04 -0500

    Filters

--- Typescript/TodoApp/src/Components/App/App.tsx
+++ Typescript/TodoApp/src/Components/App/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Todos } from '../Todos'
+import { type TodoId, type Todo as TodoType } from '../../Types/types'
 
 const todosMocks = [
   {
@@ -22,14 +23,30 @@ const todosMocks = [
 function App (): JSX.Element {
   const [todos, setTodos] = useState(todosMocks)
 
-  const handleRemove = (id: string): void => {
+  const handleRemove = ({ id }: TodoId): void => {
     const newTodos = todos.filter((todo) => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  const handleComplete = ({ id, completed }: Pick<TodoType, 'id' | 'completed'>): void => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+
+      return todo
+    })
+
     setTodos(newTodos)
   }
 
   return (
     <div className="todoapp">
       <Todos
+        handleComplete={handleComplete}
         handleRemove={handleRemove}
         todos={todos} />
     </div>
