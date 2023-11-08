10b2a8678a3128d953baaed5d79adc103d1562eb
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-11-08 16:21:44 -0500

    State update

---
+++ Typescript/traslate/src/Constants/const.ts
@@ -0,0 +1,7 @@
+export const SUPORTED_LANGUAGES = {
+  en: 'English',
+  es: 'Spanish',
+  fr: 'French'
+} as const
+
+export const AUTO_LANGUAGE = 'auto' as const
--- Typescript/traslate/src/Types/types.d.ts
+++ Typescript/traslate/src/Types/types.d.ts
@@ -1,6 +1,12 @@
+import { type AUTO_LANGUAGE, type SUPORTED_LANGUAGES } from '../Constants/const'
+
+export type Language = keyof typeof SUPORTED_LANGUAGES
+export type AuntoLanguage = typeof AUTO_LANGUAGE
+export type FromLanguage = Language | AuntoLanguage
+
 export interface State {
-  fromLanguage: string
-  toLanguage: string
+  fromLanguage: FromLanguage
+  toLanguage: Language
   fromText: string
   result: string
   loading: boolean
@@ -8,7 +14,12 @@ export interface State {
 
 export type Action =
   | { type: 'INTERCHANGE_LANGUAGES' }
-  | { type: 'SET_FROM_LANGUAGE', payload: string }
-  | { type: 'SET_TO_LANGUAGE', payload: string }
+  | { type: 'SET_FROM_LANGUAGE', payload: FromLanguage }
+  | { type: 'SET_TO_LANGUAGE', payload: Language }
   | { type: 'SET_FROM_TEXT', payload: string }
   | { type: 'SET_RESULT', payload: string }
+
+export enum SectionType {
+  From = 'from',
+  To = 'to'
+}
