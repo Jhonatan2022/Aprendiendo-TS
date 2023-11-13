b3831db608233d8371e230ecd5403b254df83766
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-11-12 21:13:09 -0500

    Traslate

---
+++ Typescript/traslate/src/Components/TextArea/index.tsx
@@ -0,0 +1,47 @@
+import { Form } from 'react-bootstrap'
+import { SectionType } from '../../Types/types'
+
+interface Props {
+  type: SectionType
+  loading?: boolean
+  onChange: (value: string) => void
+  value: string
+}
+
+const commonStyle = { border: 0, height: '200px' }
+
+const getPlaceholder = ({
+  type,
+  loading
+}: {
+  type: SectionType
+  loading?: boolean
+}) => {
+  if (type === SectionType.From) return 'Ingresa el texto a traducir'
+  if (loading === true) return 'Traduciendo...'
+  return 'Traducción'
+}
+
+const TextArea = ({ type, loading, value, onChange }: Props) => {
+  const styles =
+    type === SectionType.From
+      ? commonStyle
+      : { ...commonStyle, backgroundColor: '#f5f5f5' }
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(event.target.value)
+  }
+
+  return (
+    <Form.Control
+      autoFocus={type === SectionType.From}
+      as="textarea"
+      placeholder={getPlaceholder({ type, loading })}
+      style={styles}
+      onChange={handleChange}
+      value={value}
+    />
+  )
+}
+
+export { TextArea }
