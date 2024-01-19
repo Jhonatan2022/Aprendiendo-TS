a8b7430b888740a7fde454a1c416a1b9801b6c6c
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2024-01-18 22:01:18 -0500

    Soket

--- Node/Chat/Server/index.js
+++ Node/Chat/Server/index.js
@@ -1,15 +1,31 @@
 import express from 'express'
 import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const port = process.env.PORT ?? 1234
+
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
 app.use(logger('dev'))
-app.disable('x-powered-by')
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg)
+  })
+})
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/Client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
 })
