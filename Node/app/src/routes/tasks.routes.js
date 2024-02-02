c67e2ed829b421af9887c2d20da5455a5f7cd733
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2024-02-01 23:20:43 -0500

    Crud task

--- Node/app/src/routes/tasks.routes.js
+++ Node/app/src/routes/tasks.routes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express'
 import { authRequired } from '../middlewares/validateToken.js'
-import { createTask, deleteTask, getTask, getTasks, updateTask } from '../controllers/tasks.controller.js'
+import {
+  createTask,
+  deleteTask,
+  getTask,
+  getTasks,
+  updateTask
+} from '../controllers/tasks.controller.js'
 
 const router = Router()
 
 router.get('/tasks', authRequired, getTasks)
-router.get('/tasks/:id', authRequired, getTask)
 router.post('/tasks', authRequired, createTask)
+router.get('/tasks/:id', authRequired, getTask)
 router.put('/tasks/:id', authRequired, updateTask)
 router.delete('/tasks/:id', authRequired, deleteTask)
 
