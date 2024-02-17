ac97091729ecb861028564e5869d6fa5484eed0e
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2024-02-16 21:38:55 -0500

    Create Tasks form

---
+++ Node/app/client/src/pages/TasksFormPage.jsx
@@ -0,0 +1,38 @@
+import { useForm } from 'react-hook-form'
+
+function TasksFormPage() {
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
+  return (
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md mx-auto my-20">
+      <form action="" onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          {...register('title')}
+          className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md"
+          autoFocus
+        />
+      </form>
+      <textarea
+        rows="3"
+        placeholder="Description"
+        {...register('description')}
+        className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md"
+      ></textarea>
+
+      <button
+        className="w-full bg-zinc-500 text-white px-4 py-2 my-2 rounded-md hover:bg-blue-600"
+        type="submit"
+      >
+        Save
+      </button>
+    </div>
+  )
+}
+
+export default TasksFormPage
